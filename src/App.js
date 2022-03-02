2a6dc20363b96a819c96782c239814bb339933de
mahbub hasan <mahbubhasan2001d7@gmail.com>
2022-03-02 17:26:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const name = "Mahbub Hasan";
+  const currentTime = new Date().toLocaleTimeString();
+  const time = new Date();
+   let timeH = time.getHours();
+  let gretting = "";
+  const cssStyle = {};
+  if (timeH <= 1 && timeH > 12) {
+    gretting = "Good Morning";
+    cssStyle.color = "orange";
+  }if (timeH >= 12 && timeH < 19) {
+    gretting = "Good Afternoon";
+    cssStyle.color = "green";
+    
+  
+  } else {
+    gretting = "good Night";
+    cssStyle.color = "black";
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>My Name Is {name}</h1>
+          <h4>Current Time = {currentTime}</h4>
+          <div className="dicStyle">
+            <h2>Hello Sir , <span style ={cssStyle}> {gretting} </span></h2>
+          </div>
       </header>
     </div>
   );
